dc9268899be848f3aaa1ac8335a0653e91cf4f75
Samuca401 <matheuslealbm@gmail.com>
2024-10-12 18:23:34 -0300

    teste

--- Scripts/ScriptsOfSystem/system-extract.js
+++ Scripts/ScriptsOfSystem/system-extract.js
@@ -1,4 +1,51 @@
 // SISTEMA DE MOVIMENTAÇÕES
+const boxOfProhibited = document.querySelector("#box-of-prohibited");
+const boxOfExit = document.querySelector("#box-of-exit");
+const boxOfBalance = document.querySelector("#box-of-balance");
+
+let financeOfDates = {
+    prohibited: 0,
+    balance: 0,
+    exit: 0,
+}
+
+localDate = JSON.parse(localStorage.getItem("financeOfDates") || JSON.stringify(financeOfDates));
+
+function showFinanceDates() {
+    boxOfBalance.innerHTML = `R$ ${localDate.balance},00`;
+    boxOfProhibited.innerHTML = `R$ ${localDate.prohibited},00`;
+    boxOfExit.innerHTML = `R$ ${localDate.exit},00`;
+}
+
+function dataCalculation(type) {
+    let prohibited = localDate.prohibited;
+    let balance = localDate.balance;
+    let exit = localDate.exit;
+
+    let valor = inputValor.value;
+
+    if (type == "prohibited") {
+        prohibited += Number(valor);
+        console.log(prohibited)
+        balance = prohibited - exit;
+
+        localDate.prohibited = prohibited;
+        localDate.balance = balance;
+
+    } else if (type == "exit") {
+        exit += Number(valor);
+        balance = prohibited - exit;
+
+        localDate.exit = exit;
+        localDate.balance = balance;
+    }
+    localStorage.setItem("financeOfDates",JSON.stringify(localDate))
+
+    boxOfBalance.innerHTML = `R$ ${balance},00`;
+    boxOfProhibited.innerHTML = `R$ ${prohibited},00`;
+    boxOfExit.innerHTML = `R$ ${exit},00`;
+}
+
 
 // Acessando cards de movimento na localStorage
 let cards = JSON.parse(localStorage.getItem("cards") || "[]");
