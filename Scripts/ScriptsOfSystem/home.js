5ea69228de7ce783817c4a94e39d7493f95258d6
Samuca401 <matheuslealbm@gmail.com>
2024-10-13 00:21:12 -0300

    teste

--- Scripts/ScriptsOfSystem/home.js
+++ Scripts/ScriptsOfSystem/home.js
@@ -1,3 +1,14 @@
+let saldo = 0;
+window.addEventListener('load',()=>{
+    if (localStorage.saldo) {
+        saldo = Number(localStorage.saldo);
+        elementsDOM.saldoDOM().innerHTML = parseFloat(saldo).toFixed(2);
+    } else {
+        localStorage.saldo = saldo;
+        elementsDOM.saldoDOM().innerHTML = localStorage.saldo;
+    }
+});
+
 function acessExtracts() {
     window.location.href = "/Pages/extract.html";
 }
@@ -16,7 +27,38 @@ function expense() {
     elementsDOM.divExpense().style.display = "block";
 }
 
+function finallyOperationIncome() {
+    elementsDOM.divIncome().style.display = "none";
+
+    let value = Number(elementsDOM.inputIncomeValue().value);
+    let cause = elementsDOM.inputIncomeCause().innerHTML;
+
+    saldo += value;
+    localStorage.saldo = saldo;
+    elementsDOM.saldoDOM().textContent = parseFloat(saldo).toFixed(2);
+
+    hideLoading()
+}
+
+function finallyOperationExpense() {
+    elementsDOM.divExpense().style.display = "none";
+
+    let value = Number(elementsDOM.inputExpenseValue().value)
+    let cause = elementsDOM.inputExpenseCause().innerHTML;
+
+    saldo -= value;
+    localStorage.saldo = saldo;
+    elementsDOM.saldoDOM().textContent = parseFloat(saldo).toFixed(2);
+
+    hideLoading()
+}
+
 const elementsDOM =  {
+    saldoDOM: () => document.querySelector('#saldo'),
     divIncome: () => document.querySelector('.divIncome'),
     divExpense: () => document.querySelector('.divExpense'),
+    inputIncomeValue: () => document.querySelector('#input-income'),
+    inputIncomeCause: () => document.querySelector('#input-cause-income'),
+    inputExpenseValue: () => document.querySelector('#input-expense'),
+    inputExpenseCause: () => document.querySelector('#input-cause-expense'),
 }
